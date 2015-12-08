dbbbefb0b0d063bac02e8f384551033146ca9749
rohitsrepo@gmail.com <rohit>
2015-12-08 12:17:53 +0530

    refactor device code

--- esp_mqtt/esp_mqtt.ino
+++ esp_mqtt/esp_mqtt.ino
@@ -1,28 +1,3 @@
-/*
- Basic ESP8266 MQTT example
-
- This sketch demonstrates the capabilities of the pubsub library in combination
- with the ESP8266 board/library.
-
- It connects to an MQTT server then:
-  - publishes "hello world" to the topic "outTopic" every two seconds
-  - subscribes to the topic "inTopic", printing out any messages
-    it receives. NB - it assumes the received payloads are strings not binary
-  - If the first character of the topic "inTopic" is an 1, switch ON the ESP Led,
-    else switch it off
-
- It will reconnect to the server if the connection is lost using a blocking
- reconnect function. See the 'mqtt_reconnect_nonblocking' example for how to
- achieve the same result without blocking the main loop.
-
- To install the ESP8266 board, (using Arduino 1.6.4+):
-  - Add the following 3rd party board manager under "File -> Preferences -> Additional Boards Manager URLs":
-       http://arduino.esp8266.com/stable/package_esp8266com_index.json
-  - Open the "Tools -> Board -> Board Manager" and click install for the ESP8266"
-  - Select your ESP8266 in "Tools -> Board"
-
-*/
-
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 #include <EEPROM.h>
@@ -33,12 +8,17 @@
 
 // Update these with values suitable for your network.
 
-const char* mqtt_server = "m11.cloudmqtt.com";
-int device_id = 1;
+const char* MQTT_SERVER = "m11.cloudmqtt.com";
+const int MQTT_PORT = 12911;
+const char* MQTT_USER = "device";
+const char* MQTT_PASSWORD = "thelazypug";
+const int device_id = 1;
 
 WiFiClient espClient;
 PubSubClient client(espClient);
 
+const int ACTION_PIN = 2;
+char device_status[5];
 char target_topic[30];
 
 void setup_wifi() {
@@ -46,7 +26,15 @@ void setup_wifi() {
     wifi.autoConnect("ALAZYPUG");
 }
 
-void callback(char* topic, byte* payload, unsigned int length) {
+void publish_update() {
+  sprintf(target_topic, "devices/updates/%d", device_id);
+  Serial.println("Publish update: ");
+  Serial.println(device_status);
+  Serial.println(target_topic);
+  client.publish(target_topic, device_status);
+}
+
+void mqtt_callback(char* topic, byte* payload, unsigned int length) {
   Serial.print("Message arrived [");
   Serial.print(topic);
   Serial.print("] ");
@@ -61,23 +49,14 @@ void callback(char* topic, byte* payload, unsigned int length) {
   Serial.println("Got payload buffer");
   Serial.println(buffer);
 
-  sprintf(target_topic, "devices/updates/%d", device_id);
-
-  // Switch on the LED if an 1 was received as first character
   if (strstr(buffer, "off")) {
-    digitalWrite(2, LOW);   // Turn the LED on (Note that LOW is the voltage level
-    // but actually the LED is on; this is because
-    // it is acive low on the ESP-01)
-
-    Serial.println("Publish update:off to");
-    Serial.println(target_topic);
-    client.publish(target_topic, "off");
-    
+    digitalWrite(ACTION_PIN, LOW);
+    strncpy(device_status, "off", sizeof("off"));
+    publish_update();
   } else if (strstr(buffer, "on")) {
-    digitalWrite(2, HIGH);  // Turn the LED off by making the voltage HIGH
-    client.publish(target_topic, "on");
-    Serial.println("Publish update:on to");
-    Serial.println(target_topic);
+    digitalWrite(ACTION_PIN, HIGH);
+    strncpy(device_status, "on", sizeof("on"));
+    publish_update();
   } else {
     Serial.print("Got invalid action:");
   }
@@ -85,36 +64,45 @@ void callback(char* topic, byte* payload, unsigned int length) {
 }
 
 void setup() {
-  pinMode(2, OUTPUT);     // Initialize the BUILTIN_LED pin as an output
+  pinMode(ACTION_PIN, OUTPUT);
   Serial.begin(115200);
   setup_wifi();
-  client.setServer(mqtt_server, 12911);
-  client.setCallback(callback);
+  strncpy(device_status, "off", sizeof("off"));
+  client.setServer(MQTT_SERVER, MQTT_PORT);
+  client.setCallback(mqtt_callback);
 }
 
-void reconnect() {
+void publish_connect() {
+  Serial.println("connected");
+  
+  sprintf(target_topic, "devices/connects/%d", device_id);
+  Serial.println("Publish connect to");
+  Serial.println(target_topic);
+
+  String localIp = String(WiFi.localIP()[0]) + "." + 
+  String(WiFi.localIP()[1]) + "." + 
+  String(WiFi.localIP()[2]) + "." + 
+  String(WiFi.localIP()[3]);
+  client.publish(target_topic, localIp.c_str());
+}
+
+void subscribe_topics() {
+  sprintf(target_topic, "devices/actions/%d", device_id);
+      
+  Serial.println("Subscribe to");
+  Serial.println(target_topic);
+  client.subscribe(target_topic);
+}
+
+void mqtt_reconnect() {
   // Loop until we're reconnected
   while (!client.connected()) {
     Serial.print("Attempting MQTT connection...");
     // Attempt to connect
-    if (client.connect("ESP8266Client", "device", "thelazypug")) {
-      Serial.println("connected");
-      // Once connected, publish an announcement...
-      sprintf(target_topic, "devices/connects/%d", device_id);
-      Serial.println("Publish connect to");
-      Serial.println(target_topic);
-
-      String localIp = String(WiFi.localIP()[0]) + "." + 
-        String(WiFi.localIP()[1]) + "." + 
-        String(WiFi.localIP()[2]) + "." + 
-        String(WiFi.localIP()[3]);
-      client.publish(target_topic, localIp.c_str());
-      // ... and resubscribe
-      sprintf(target_topic, "devices/actions/%d", device_id);
-      
-      Serial.println("Subscribe to");
-      Serial.println(target_topic);
-      client.subscribe(target_topic);
+    if (client.connect("ESP8266Client", MQTT_USER, MQTT_PASSWORD)) {
+      publish_connect();
+      publish_update();
+      subscribe_topics();
     } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
@@ -124,10 +112,11 @@ void reconnect() {
     }
   }
 }
+
 void loop() {
 
   if (!client.connected()) {
-    reconnect();
+    mqtt_reconnect();
   }
   client.loop();
 }
